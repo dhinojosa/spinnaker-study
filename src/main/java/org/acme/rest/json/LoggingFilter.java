0b32cda94a551b068565b5270a912d84934fe423
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2020-01-20 18:59:27 -0700

    initial project

---
+++ src/main/java/org/acme/rest/json/LoggingFilter.java
@@ -0,0 +1,32 @@
+package org.acme.rest.json;
+
+import io.vertx.core.http.HttpServerRequest;
+import org.jboss.logging.Logger;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class LoggingFilter implements ContainerRequestFilter {
+
+    private static final Logger LOG = Logger.getLogger(LoggingFilter.class);
+
+    @Context
+    UriInfo info;
+
+    @Context
+    HttpServerRequest request;
+
+    @Override
+    public void filter(ContainerRequestContext context) {
+
+        final String method = context.getMethod();
+        final String path = info.getPath();
+        final String address = request.remoteAddress().toString();
+
+        LOG.infof("Request %s %s from IP %s", method, path, address);
+    }
+}
---
+++ src/test/java/org/acme/rest/json/LegumeResourceIT.java
@@ -0,0 +1,8 @@
+package org.acme.rest.json;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class LegumeResourceIT extends LegumeResourceTest {
+
+}
