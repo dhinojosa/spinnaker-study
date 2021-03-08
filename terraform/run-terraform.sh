9c72cf231490100ff62091951d910001b8de1deb
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2021-03-08 13:07:33 -0700

    added new terraform script

--- terraform/run-terraform.sh
+++ terraform/run-terraform.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-
-echo $AWS_ACCESS_KEY_ID
-echo $AWS_SECRET_ACCESS_KEY
-
 terraform init
 terraform apply -auto-approve
+
+# Output the results so we can use it within Spinnaker
+# All outputs must be prefixed with SPINNAKER_PROPERTY
+
 echo "SPINNAKER_PROPERTY_DB_ADDRESS=$(terraform output db_address)"
 echo "SPINNAKER_PROPERTY_DB_NAME=$(terraform output db_name)"
 echo "SPINNAKER_PROPERTY_DB_SECRET=$(terraform output db_secret)"
