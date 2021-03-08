0571c8105d1df9b27293fc5ef49db2587b20572b
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2021-03-08 15:04:18 -0700

    made a better document set up, renamed files

---
+++ terraform/run.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+terraform init
+terraform apply -auto-approve
+
+# Output the results so we can use it within Spinnaker
+# All outputs must be prefixed with SPINNAKER_PROPERTY
+
+echo "SPINNAKER_PROPERTY_DB_ADDRESS=$(terraform output db_address | tr -d '"')"
+echo "SPINNAKER_PROPERTY_DB_NAME=$(terraform output db_name | tr -d '"')"
+echo "SPINNAKER_PROPERTY_DB_SECRET=$(terraform output db_secret | tr -d '"')"
+echo "SPINNAKER_PROPERTY_DB_USERNAME=$(terraform output db_username | tr -d '"')"
+echo "SPINNAKER_PROPERTY_COOL=wow"
