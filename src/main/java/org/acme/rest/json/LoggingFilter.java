defc65cef99c7fbae826c9c9bbcfcfbea5b2ff84
sgovinda-ub <shyirtt@gmail.com>
2023-01-11 07:40:01 +0530

    Update LoggingFilter.java

--- src/main/java/org/acme/rest/json/LoggingFilter.java
+++ src/main/java/org/acme/rest/json/LoggingFilter.java
@@ -32,5 +32,7 @@ public class LoggingFilter implements ContainerRequestFilter {
                 
         LOG.infof("Request %s %s from IP %s", method, path, address);
         LOG.infof("Request %s %s from IP %s", method, path, address);
+        
+        LOG.infof("Request %s %s from IP %s", method, path, address);
     }
 }
